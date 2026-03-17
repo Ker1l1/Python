3c8ec53badf0ecd957b9c616c39e518517ae0af3
Ker1l1 <sweeetboes@gmail.com>
2026-03-17 17:52:05 +0500

    Калькулятор

---
+++ main.py
@@ -0,0 +1,23 @@
+try:
+    num_1 = float(input("Введите число: "))
+    num_2 = float(input("Введите число: "))
+    op = input("Введите операцию (+, -, /, *): ")
+    if op == "+":
+        result = num_1 + num_2
+    elif op == "-":
+        result = num_1 - num_2
+    elif op == "/":
+        result = num_1 / num_2
+    elif op == "*":
+        result = num_1 * num_2
+    else:
+        print("Ошибка")
+        result = None
+    if result is not None:
+        print(f"Результат:  {result}")
+except ValueError:
+    print("Ошибка: введено не число!")
+except ZeroDivisionError:
+    print("Ошибка: деление на ноль!")
+finally:
+    print("Подсчет заверешен")
