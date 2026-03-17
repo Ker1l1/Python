84efb0e3462167d5cf5cbade8ecfc5be83b72787
Ker1l1 <sweeetboes@gmail.com>
2026-03-17 19:10:50 +0500

    Калькулятор

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ try:
     num_1 = float(input("Введите число: "))
     num_2 = float(input("Введите число: "))
     op = input("Введите операцию (+, -, /, *): ")
-    if op == "+":
+    if   op == "+":
         result = num_1 + num_2
     elif op == "-":
         result = num_1 - num_2
@@ -13,8 +13,7 @@ try:
     else:
         print("Ошибка")
         result = None
-    if result is not None:
-        print(f"Результат:  {result}")
+    print(f"Результат:  {result}")
 except ValueError:
     print("Ошибка: введено не число!")
 except ZeroDivisionError:
